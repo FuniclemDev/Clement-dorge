5cd4b6406c5b35ffddacd8384768f7f554c7326e
ClemDev <clement.dorge@epitech.eu>
2024-07-19 11:37:58 +0200

    [+] My Paint project

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// Fonction pour afficher les informations du projet au survol
-function showProjectInfo(element) {
-    element.querySelector('.project-info').style.opacity = '1';
-}
-
-// Fonction pour masquer les informations du projet lorsque le survol est terminé
-function hideProjectInfo(element) {
-    element.querySelector('.project-info').style.opacity = '0';
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const presentationSection = document.querySelector('.presentation');
 
